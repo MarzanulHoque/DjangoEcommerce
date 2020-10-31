68491c969d9a480c232d0cee5ddff8a63ae28813
marzanulhoque <marzanulru17@gmail.com>
2020-10-31 09:37:49 +0600

    Creating Order Tracker For Customers

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
+# Create your models here.
 class Product(models.Model):
-    product_id=models.AutoField
+    product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    desc = models.CharField(max_length=300)
-    category=models.CharField(max_length=50,default="")
-    subcategory = models.CharField(max_length=50,default="")
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
+    desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to="shop/images",default="")
+    image = models.ImageField(upload_to='shop/images', default="")
 
     def __str__(self):
         return self.product_name
-    
+
+
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -25,16 +27,21 @@ class Contact(models.Model):
         return self.name
 
 class Orders(models.Model):
-    order_id= models.AutoField(primary_key=True)
-    items_json= models.CharField(max_length=5000)
-    name=models.CharField(max_length=90)
-    email=models.CharField(max_length=111)
-    address=models.CharField(max_length=111)
-    city=models.CharField(max_length=111)
-    state=models.CharField(max_length=111)
-    zip_code=models.CharField(max_length=111)
-    phone = models.CharField(max_length=20,default="")
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zip_code = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111, default="")
+
+class OrderUpdate(models.Model):
+    update_id  = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-
+        return self.update_desc[0:7] + "..."
